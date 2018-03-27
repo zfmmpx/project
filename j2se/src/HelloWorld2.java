c4bfb12dd005dbd80a04b9851cbe23352bbb9277
JimmyFromMBP <zfmmpx@gmail.com>
2018-03-27 13:48:50 +0800

    gy

--- j2se/src/HelloWorld2.java
+++ j2se/src/HelloWorld2.java
@@ -1,16 +1,25 @@
 public class HelloWorld2 {
-	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
-			boolean breakout = false;
-			for (int j = 0; j < 10; j++) {
-				System.out.println(i + ":" + j);
-				if (0 == j % 2) {
-					breakout = true;
-					break;
+	public static void main (String[] args) {
+		float goldPoint = 0.618f;
+		float cache = 30f;
+		float result;
+		float I = 0;
+		float J = 0;
+		for (int i = 1; i < 21; i++) {
+			for(int j = 1; j < 21; j++) {
+				if (0 == i % 2 && 0 == j % 2) {
+					continue;
+				}
+				result = Math.abs((float)i / j - goldPoint);
+				if (result < cache) {
+					cache = result;
+					I = i;
+					J = j;
+					System.out.println("20以内两个数相除,最接近黄金分割点的是: " + I + "/" + J + ",值为:" + (cache + goldPoint));
+					System.out.println("result = " + result);
 				}
 			}
-			if(breakout)
-				break;
 		}
+		System.out.println("20以内两个数相除,最接近黄金分割点的是: " + I + "/" + J + ",值为:" + (I / J));
 	}
 }
